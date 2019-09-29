aabb4f182e3547e404f8ffcbe0b63ba947dc38b9
romanlevonovyy <32957243+romanlevonovyy@users.noreply.github.com>
2019-09-29 23:54:53 +0300

    Lab_1

---
+++ Lab1_rollaboll/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyUp("space") && GetComponent<Rigidbody>().transform.position.y <= 0.5f)
+        {
+            Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
+            GetComponent<Rigidbody>().AddForce(jump);
+            jump = new Vector3(0.0f, -200.0f, 0.0f);
+
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+    
+}
---
+++ Lab1_rollaboll/README.md
@@ -0,0 +1,2 @@
+# Unity
+Repository for "Computer Graphic" course
